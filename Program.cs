ba55e7e4f0499507a945f87720aa2c2cd542f495
eloa <antuneseloa23@gmail.com>
2025-10-24 20:45:20 -0300

    atividade finalizada!

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿bool t, p, r, w;
+
+Console.WriteLine("--- Simulador de luz de Advergência...: ");
+
+Console.Write("sensor de températura...: ");
+
+string sensorT = Console.ReadLine()!.ToUpper();
+t = (sensorT == "A");
+
+Console.Write("sensor de pressão...: ");
+string sensorP = Console.ReadLine()!.ToUpper();
+p = (sensorT == "A");
+
+Console.Write("sensor de rotação...: ");
+string sensorR = Console.ReadLine()!.ToUpper();
+r = (sensorT == "A");
+
+w = t && (p || !r);
+
+string resultado;
+
+if (w)
+{
+    resultado = "ATIVADA";
+}
+resultado = w ? "ATIVADA" : "DESATIVADA";
+
+Console.WriteLine($"\nA luz de advertência estará {resultado}.");
+
